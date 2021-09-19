8686710b455109f4e298fa01cc1a1fc3e391ed50
Yacine Merzouk <yacine@merzouk.ca>
2021-09-18 15:28:16 -1000

    Updated for Meteor 2.3.6

--- package.js
+++ package.js
@@ -1,39 +1,39 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-controller',
   summary: 'Fork of iron:controller for Firestorm',
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-controller'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@2.2');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@2.3.6');
 
   api.use('underscore');
   api.use('tracker'); // reactivity
   api.use('reactive-dict'); // reactive state variables
   api.use('templating@1.4.0');
 
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.4');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.5');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.5');
 
-  api.use('meteorhubdotnet:firestorm-iron-layout@1.0.4');
-  api.use('meteorhubdotnet:firestorm-iron-dynamic-template@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-layout@1.0.5');
+  api.use('meteorhubdotnet:firestorm-iron-dynamic-template@1.0.5');
 
-  api.add_files('lib/wait_list.js', 'client');
-  api.add_files('lib/controller.js');
-  api.add_files('lib/controller_server.js', 'server');
-  api.add_files('lib/controller_client.js', 'client');
+  api.addFiles('lib/wait_list.js', 'client');
+  api.addFiles('lib/controller.js');
+  api.addFiles('lib/controller_server.js', 'server');
+  api.addFiles('lib/controller_client.js', 'client');
 });
 
-Package.on_test(function (api) {
-  api.use('meteorhubdotnet:firestorm-iron-controller@1.0.4');
-  api.use('meteorhubdotnet:firestorm-iron-layout@1.0.4');
+Package.onTest(function (api) {
+  api.use('meteorhubdotnet:firestorm-iron-controller@1.0.5');
+  api.use('meteorhubdotnet:firestorm-iron-layout@1.0.5');
   api.use('tinytest');
   api.use('test-helpers');
   api.use('tracker');
   api.use('templating');
 
-  api.add_files('test/controller_test.html', 'client');
-  api.add_files('test/wait_list_test.js', 'client');
-  api.add_files('test/controller_test.js', 'client');
+  api.addFiles('test/controller_test.html', 'client');
+  api.addFiles('test/wait_list_test.js', 'client');
+  api.addFiles('test/controller_test.js', 'client');
 });
